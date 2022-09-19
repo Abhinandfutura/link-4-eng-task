9df885131678b846fbf830f159ca9600b6d9fd9b
Abhinand P <abhinandnadupparambil@gmail.com>
2022-09-19 16:18:57 -0400

    forth push

--- src/Pages/CarDetailes.js
+++ src/Pages/CarDetailes.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components/macro";
 import axios from "axios";
 import { useParams } from "react-router-dom";
